7ec149a62e7f45d5afb240fb7942b0ad7bc913d9
KisaZP <56171651+KisaZP@users.noreply.github.com>
2019-12-24 00:23:20 +0200

    Add files via upload

---
+++ stroka37.py
@@ -0,0 +1,9 @@
+strin = input('Введите строку:')
+strin = list(strin)
+x = len(strin)
+for y in range (1,3):
+    print(strin, x)
+    print('символов, ')
+for y in range (0,1):
+    print(strin, x)
+    print('символов. ')
